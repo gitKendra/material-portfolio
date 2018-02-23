79684ae96a15ba18d049e47fc6676097777835f6
gitKendra <kendra.krzywicki@knights.ucf.edu>
2018-02-22 20:11:39 -0500

    all commit

--- js/init.js
+++ js/init.js
@@ -159,12 +159,6 @@
 		$newWord.removeClass('is-hidden').addClass('is-visible');
 	}
 
-	// $('.button-collapse').sideNav({
-	// 	menuWidth: 240, // Default is 240
-	// 	closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-	// });
-
-	
 	$('.parallax').parallax();
 
 	var card  = document.querySelectorAll('.card-work');
